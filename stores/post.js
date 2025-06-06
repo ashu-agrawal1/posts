482dd5739fc86fae67cd52e84ae9bfe57d0c5633
Ashu <ashuagrawal3000@gmail.com>
2025-06-06 16:43:04 +0530

    caching logic for 15 mins

--- stores/post.js
+++ stores/post.js
@@ -1,9 +1,14 @@
 export const usePostStore = defineStore("post", () => {
   const posts = ref([]);
+  const lastFetched = ref(0);
 
   const fetchPosts = async () => {
+    const now = Date.now();
+    const fifteenMin = 15 * 60 * 1000;
+    if (posts.value.length && now - lastFetched.value < fifteenMin) return;
     const { data } = await useFetch("https://dummyjson.com/posts");
     posts.value = data.value.posts;
+    lastFetched.value = now;
   };
 
   return { posts, fetchPosts };
