e0b5b61153a8b51d203f606e21eedf19f0ddf4d6
Ashu <ashuagrawal3000@gmail.com>
2025-06-06 16:20:06 +0530

    dynamic route for post details page

---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold mb-4">{{ post?.title }}</h1>
+    <p class="text-gray-700">{{ post?.body }}</p>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const { data: post } = await useFetch(
+  `https://dummyjson.com/posts/${route.params.id}`
+);
+</script>
